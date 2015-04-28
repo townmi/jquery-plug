0df630fae737006a16ae0cca6b145d71101ae0f2
townmi <towne766@126.com>
2015-04-29 03:41:28 +0800

    add grid

---
+++ cms/dev/js/apps/grid.js
@@ -0,0 +1,17 @@
+define(function(require, exports, module){
+
+    var $ = require("jquery");
+
+    var datatables = require("datatables")($);
+
+    var datatables_ext = require("datatables_ext")($,datatables);
+
+    $(document).ready(function() {
+
+        $('#dataTables-example').DataTable({
+           responsive: true
+        });
+        
+    });
+
+});
